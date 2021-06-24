564b2ad9bf94194999d30aef958c96b60934bfaa
Ryan Tan <oathyu@gmail.com>
2021-06-25 00:36:43 +0800

    Training Mode Feedback Commit
    Added feedback portion for training mode

--- electron-app/preload.js
+++ electron-app/preload.js
@@ -3,7 +3,6 @@ const { ipcRenderer, ipcMain } = require('electron')
 const httpHandler = require('./httpHandler.js')
 
 /*TODO:
-    Set up feedback (Priority)
     Set up dashboard and login page
     Implement testing
 */
@@ -11,7 +10,7 @@ const httpHandler = require('./httpHandler.js')
 var stockTicker = null;
 
 //Generate the three fundamental indicators to assess
-function generateFundamentalScenario(dataObject, callBack) {
+function generateFundamentalScenario(dataObject) {
 
     let array = []
     let max = 20
@@ -82,23 +81,186 @@ function generateFundamentalScenario(dataObject, callBack) {
                 break
         }
         if (array.indexOf(string) === -1) {
-            if (!(dataObject[string] === undefined || dataObject[string] === null)) {
+            if (!(dataObject[string] === undefined || dataObject[string] === '')) {
                 console.log("Chosen Indicator: " + string)
                 console.log("Chosen Indicator Value: " + dataObject[string])
                 array.push(string)
             }
         }
     }
-    callBack(array)
-    array1 = array
+    return array
 }
 
 //Display the radio buttons corresponding to the indicators that the scenario generator produced
 function displayFundamentalScenario(array) {
     for (let i = 0; i < array.length; i++) {
-        indicatorName = array[i];
-        element = document.getElementById(indicatorName);
+        let indicatorName = array[i];
+        let element = document.getElementById(indicatorName);
         element.style.display = "initial"
+    }
+}
+
+function hideFundamentalScenario(array) {
+    for (let i = 0; i < array.length; i++) {
+        let indicatorName = array[i];
+        let element = document.getElementById(indicatorName);
+        element.style.display = "none"
+    }
+}
+
+//Set up the submit button for fundamental user input and the callback to processs data
+function setupSubmitButton(array, callBack) {
+    let submitButton = document.getElementById("submitButton")
+    submitButton.onclick = async(e) => {
+        let indicatorArray = Array.from(array)
+        let dataObject = {
+            'stockTicker': stockTicker,
+            'indicators' : indicatorArray
+        }
+        for (let i = 0; i < array.length; i++) {
+            indicatorName = array[i]
+            elements = document.getElementsByName(indicatorName)
+            for (let j = 0; j < elements.length; j++) {
+                radioButton = elements[j]
+                if (radioButton.checked) {
+                    //Make a Javascript object with key value pair, with indicator name as key and strength as value
+                    dataObject[indicatorName] = radioButton.nextElementSibling.innerText 
+                }
+            }
+        }
+
+        let pricePrediction = document.getElementsByName("PricePrediction")
+        for (let i = 0; i < pricePrediction.length; i++) {
+            radioButton = pricePrediction[i]
+            if (radioButton.checked) {
+                dataObject['pricePrediction'] = radioButton.nextElementSibling.innerText
+            }
+        }
+
+        //Pass the compiled dataObject containing user inputs to the callBack for processing
+        let compiledDataObject = callBack(dataObject)
+        let feedbackDataObject = generateFeedback(compiledDataObject)
+        hideFundamentalScenario(indicatorArray)
+        displayFeedBack(feedbackDataObject)
+    }
+}
+
+//Function to store correlation dataObject, while waiting for the user dataObject
+function storeDataObjects(correlationDataObject) {
+    return (userDataObject) => {
+        compiledDataObject = {
+            'correlationDataObject': correlationDataObject,
+            'userDataObject' : userDataObject
+        }
+        return compiledDataObject
+    }
+}
+
+//Processes the compiled userDataObject and correlationDataObject to generate feedback values
+function generateFeedback(compiledDataObject) {
+    let userDataObject = compiledDataObject['userDataObject']
+    let correlationDataObject = compiledDataObject['correlationDataObject']
+    let pricePrediction = userDataObject['pricePrediction']
+
+    let feedbackDataObject = {
+        'indicators': Array.from(userDataObject['indicators'])
+    }
+
+    for (let i = 0; i < userDataObject['indicators'].length; i++) {
+        let indicatorName = userDataObject['indicators'][i]
+        let indicatorStrength = userDataObject[indicatorName]
+        let indicatorCorrelationValue = correlationDataObject[indicatorName]
+
+        let indicatorCorrelationStrength = null
+        if (indicatorCorrelationValue <= 0) {
+            indicatorCorrelationStrength = 'Weak'
+        } else if (indicatorCorrelationValue < 0.5 && indicatorCorrelationValue > 0) {
+            indicatorCorrelationStrength = 'Medium'
+        } else {
+            indicatorCorrelationStrength = 'Strong'
+        }
+
+        if (indicatorCorrelationStrength == indicatorStrength) {
+            feedbackDataObject[indicatorName] = {
+                'feedback': "Good! Your evaluation of this fundamental indicator matches our stock evaluator!",
+                'indicatorCorrelationStrength': indicatorCorrelationStrength,
+                'indicatorStrength': indicatorStrength
+            }
+        } else {
+            feedbackDataObject[indicatorName] = {
+                'feedback': "Unfortunately, your evaluation of this fundamental indicator does not match our stock evaluator.",
+                'indicatorCorrelationStrength' : indicatorCorrelationStrength,
+                'indicatorStrength': indicatorStrength
+            }
+        }
+
+        //Logging for debugging purposes
+        console.log("Indicator Strength: " + indicatorStrength)
+        console.log("Indicator Name: " + indicatorName)
+        console.log("Indicator Correlation Value: " + indicatorCorrelationValue)
+
+        //Logging feedback object for sanity check
+        console.log("Feedback: " + feedbackDataObject[indicatorName]['feedback'])
+    }
+    console.log("User Price Prediction: " + pricePrediction)
+
+    return feedbackDataObject
+}
+
+//Displays the feedback section and hides all other sections in training section, then dynamically modify the DOM
+function displayFeedBack(feedbackDataObject) {
+    let infobody = document.getElementById("infobody")
+    let infocontent = document.getElementById("infocontent")
+    let infofeedback = document.getElementById("infofeedback")
+    infobody.style.display = "none"
+    infocontent.style.display = "none"
+    infofeedback.style.display = "initial"
+
+    for (let i = 0; i < feedbackDataObject['indicators'].length; i++) {
+        let indicatorName = feedbackDataObject['indicators'][i]
+
+        //Show user's evalution combined with our feedback on it
+        let header = document.getElementById("userinput" + i.toString() + "header")
+        header.innerText = indicatorName
+            .replace(/([A-Z])/g, ' $1')
+            .replace(/^./, function(str){ return str.toUpperCase(); })
+        let indicatorStrength = feedbackDataObject[indicatorName]['indicatorStrength']
+
+        let elements = document.getElementsByName("userinput" + i.toString())
+        for (let j = 0; j < elements.length; j++) {
+            let radioButton = elements[j]
+            let label = radioButton.nextElementSibling
+            if (indicatorStrength == label.innerText) {
+                radioButton.checked = true
+            } else {
+                radioButton.disabled = true
+                label.disabled = true
+            }
+        }
+
+        let feedback = feedbackDataObject[indicatorName]['feedback']
+        let p = document.getElementById("userinput" + i.toString() + "feedback")
+        p.innerText = feedback
+
+        //Show stock evaluator's evaluation
+        header = document.getElementById("input" + i.toString() + "header")
+        header.innerText = indicatorName
+            .replace(/([A-Z])/g, ' $1')
+            .replace(/^./, function(str){ return str.toUpperCase(); })
+
+        let indicatorCorrelationStrength = feedbackDataObject[indicatorName]['indicatorCorrelationStrength']
+
+        elements = document.getElementsByName("input" + i.toString())
+        for (let j = 0; j < elements.length; j++) {
+            radioButton = elements[j]
+            label = radioButton.nextElementSibling
+            if (indicatorCorrelationStrength == label.innerText) {
+                radioButton.checked = true
+            } else {
+                radioButton.disabled = true
+                label.disabled = true
+            }
+        }
     }
 }
 
@@ -191,7 +353,10 @@ ipcRenderer.on('priceData', (event, message) => {
 
 //When receiving a message on this channel, generate fundamental scenario
 ipcRenderer.on('correlationData', (event, message) => {
-    generateFundamentalScenario(message, displayFundamentalScenario)
+    let array = generateFundamentalScenario(message)
+    displayFundamentalScenario(array)
+    storeUserDataObject = storeDataObjects(message)
+    setupSubmitButton(array, storeUserDataObject)
 })
 
 //Set up collapsible button for the tables
@@ -254,7 +419,7 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 })
 
-//Handle year and stock form submissions, behavior for the get data button, as well as the submit button to get user input
+//Handle year and stock form submissions, behavior for the getData button, as well as begin chain to set up fundamental scenario
 window.addEventListener('DOMContentLoaded', () => {
     
     let array = []
@@ -342,39 +507,17 @@ window.addEventListener('DOMContentLoaded', () => {
         ipcRenderer.send('priceData', array)
     }
 
-    //Set up the submit button for fundamental user input, for backend to process
-    let submitButton = document.getElementById("submitButton")
-    submitButton.onclick = async(e) => {
-        let indicatorArray = Array.from(array1)
-        let dataObject = {
-            'stockTicker': stockTicker,
-            'indicators' : indicatorArray
-        }
-        for (let i = 0; i < array1.length; i++) {
-            indicatorName = array1[i]
-            elements = document.getElementsByName(indicatorName)
-            for (let j = 0; j < elements.length; j++) {
-                radioButton = elements[j]
-                if (radioButton.checked) {
-                    //Make a Javascript object with key value pair, with indicator name as key and strength as value
-                    dataObject[indicatorName] = radioButton.nextElementSibling.innerText 
-                }
-            }
-        }
-
-        pricePrediction = document.getElementsByName("PricePrediction")
-        for (let i = 0; i < pricePrediction.length; i++) {
-            radioButton = pricePrediction[i]
-            if (radioButton.checked) {
-                dataObject['pricePrediction'] = radioButton.nextElementSibling.innerText
-            }
-        }   
-
-        //Testing to see if it works => works
-        for (let i = 0; i < array1.length; i++) {
-            console.log(dataObject[indicatorArray[i]])
-            console.log(dataObject['indicators'][i])
-        }
-        console.log(dataObject['pricePrediction'])
+    //Set up finishButton for feedback portion => implement resetting of all variables
+    let finishButton = document.getElementById("finishButton")
+    finishButton.onclick = async(e) => {
+        let infofeedback = document.getElementById("infofeedback")
+        infofeedback.style.display = "none"
+        let form = document.getElementById("stockform")
+        form.style.display = "block"
+        form.value = ""
+        replaceText("stock", "Stock:")
+        let beginButton = document.getElementById("beginButton")
+        beginButton.style.margin = "1em 0 4em 0"
+        beginButton.style.display = "block"
     }
 })
